935e3e86992f20a84eb2d209ee892d74e9941a2a
wangzlab <51474812+wangzlab@users.noreply.github.com>
2022-11-15 16:07:39 +0800

    Update calculate_AMHI.pl

--- AMHI calculation Figure 4/calculate_AMHI.pl
+++ AMHI calculation Figure 4/calculate_AMHI.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 # This scripts calculate AMHI by adopting a slightly modified scheme from that of Gupta and colleagues to calculate GMHI. 
-# Both bacterial and fungal profiles are used and the thresholds for their fold-changes were simutaneously optimized.
+# Both bacterial and fungal profiles were used and the thresholds for their fold-changes were simutaneously optimized.
 
 open (IN, "bacteria_name_mapping");
 while (<IN>) {
